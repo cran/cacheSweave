707484feede69618cc2b0aec2bc3a96287fd4cb5
Roger D. Peng <rpeng@jhsph.edu>
2007-05-16 00:00:00 +0000

    version 0.4-2

--- tests/reg-tests.R
+++ tests/reg-tests.R
@@ -1,7 +1,18 @@
-library(cacheSweave)
+suppressMessages(library(cacheSweave))
 
 infile <- system.file("example", "simpleRR.Rnw", package = "cacheSweave")
 
 setCacheDir("cache")
 
 Sweave(infile, driver = cacheSweaveDriver)
+
+ls()
+
+
+## Check to see that objects were properly cached
+## Database should contain 'airquality' and 'fit'
+
+dname <- dir("cache", full.names = TRUE)
+suppressMessages(library(stashR))
+db <- new("localDB", dir = dname, name = "cacheDB")
+dbList(db)
